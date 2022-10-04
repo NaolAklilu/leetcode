fc7ae60e360ec7c2838c335306153e2e966a26cd
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2022-10-05 01:40:50 +0300

    Create find_the_student_that_will_replace_the_chalk_1894.py

---
+++ find_the_student_that_will_replace_the_chalk_1894.py
@@ -0,0 +1,9 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        k %= sum(chalk)
+                     
+        for i in range(len(chalk)):
+            if k - chalk[i] < 0:
+                return i
+            else:
+                k -= chalk[i]
